17235a97a27b5ecfd89304517cc041de94eea4b1
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-07-15 23:35:20 +0530

    Feat: Add or remove the class based on the state of the video

--- src/player.js
+++ src/player.js
@@ -1,4 +1,5 @@
 const playPauseBtn = document.querySelector(".play-pause-btn")
+const videoContainer = document.querySelector(".video-container")
 const video = document.querySelector("video")
 
 const togglePlay = () => {
@@ -6,3 +7,11 @@ const togglePlay = () => {
 }
 
 playPauseBtn.addEventListener("click", togglePlay)
+
+video.addEventListener("play", () => {
+  videoContainer.classList.remove("paused")
+})
+
+video.addEventListener("pause", () => {
+  videoContainer.classList.add("paused")
+})
