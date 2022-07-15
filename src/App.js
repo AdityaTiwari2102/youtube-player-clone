05d575796fc8b9a94f50a43ca3244c83ac48d65d
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-07-15 22:30:06 +0530

    Chore: Remove the unwanted HTML code

--- src/App.js
+++ src/App.js
@@ -4,23 +4,6 @@ import { faCoffee, fa4 } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <FontAwesomeIcon icon={ faCoffee } />
-        <FontAwesomeIcon icon={ fa4 } />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
