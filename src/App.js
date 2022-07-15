6e08c8af126ad4ce98df96d35b5e202d48b0a505
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-07-15 22:33:44 +0530

    Feat: Video player with play and pause button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import video from './assets/Video.mp4';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, fa4 } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
+    <div className="video-container">
+      <div className="video-controls-container">
+        <div className='timeline-container'>
+
+        </div>
+        <div className='controls'>
+          <button className="play-pause-btn">
+          </button>
+        </div>
+      </div>
+      <video src={video}></video>
     </div>
   );
 }
