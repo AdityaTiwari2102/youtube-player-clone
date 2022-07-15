7caedf759c5b03b05372557bd9a3235211aa2e81
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-07-15 23:36:23 +0530

    Chore: Render the JS with onload

--- src/player.js
+++ src/player.js
@@ -1,17 +1,19 @@
-const playPauseBtn = document.querySelector(".play-pause-btn")
-const videoContainer = document.querySelector(".video-container")
-const video = document.querySelector("video")
+window.onload=function(){
+  const playPauseBtn = document.querySelector(".play-pause-btn")
+  const videoContainer = document.querySelector(".video-container")
+  const video = document.querySelector("video")
 
-const togglePlay = () => {
-  video.paused ? video.play() : video.pause()
+  const togglePlay = () => {
+    video.paused ? video.play() : video.pause()
+  }
+
+  playPauseBtn.addEventListener("click", togglePlay)
+
+  video.addEventListener("play", () => {
+    videoContainer.classList.remove("paused")
+  })
+
+  video.addEventListener("pause", () => {
+    videoContainer.classList.add("paused")
+  })
 }
-
-playPauseBtn.addEventListener("click", togglePlay)
-
-video.addEventListener("play", () => {
-  videoContainer.classList.remove("paused")
-})
-
-video.addEventListener("pause", () => {
-  videoContainer.classList.add("paused")
-})
