c99044ad5c0f937cf21ec5cbc5b272403abbaf19
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-07-15 20:40:07 +0530

    Font Awesome setup (#1)
    * Chore: Packages for font awesome icons
    
    * Chore: Add dummy FA icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, fa4 } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <FontAwesomeIcon icon={ faCoffee } />
+        <FontAwesomeIcon icon={ fa4 } />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
