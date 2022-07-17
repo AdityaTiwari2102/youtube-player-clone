7c35bfc408c1872e2ab15fad47f3ca2f7b2f6a50
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-07-17 15:52:28 +0530

    Feat: JS to toggle full screen mode

--- src/player.js
+++ src/player.js
@@ -1,15 +1,30 @@
 window.onload=function(){
   const playPauseBtn = document.querySelector(".play-pause-btn")
   const theaterBtn = document.querySelector(".theater-btn")
+  const fullScreenBtn = document.querySelector(".full-screen-btn")
   const videoContainer = document.querySelector(".video-container")
   const video = document.querySelector("video")
 
   // View mode
   theaterBtn.addEventListener("click", toggleTheaterMode)
+  fullScreenBtn.addEventListener("click", toggleFullScreenMode)
 
   function toggleTheaterMode() {
     videoContainer.classList.toggle("theater")
   }
+
+  function toggleFullScreenMode() {
+    if (document.fullscreenElement == null) {
+      videoContainer.requestFullscreen()
+    } else {
+      document.exitFullscreen()
+    }
+  }
+
+  document.addEventListener("fullscreenchange", () => {
+    videoContainer.classList.toggle("full-screen", document.fullscreenElement)
+  })
+
   // Play or pause 
   const togglePlay = () => {
     video.paused ? video.play() : video.pause()
