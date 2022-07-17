bb0fb0821e6a673a29a47e387e7367afb5e8f6f8
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-07-17 19:17:11 +0530

    Fullscreen, theater, and mini player functionalities (#4)
    * Chore: Replace the FA icons with SVG
    
    * Chore: FA icons for full screen button
    
    * Refactor: Replace the FA icons with SVG
    
    * Chore: Mini player button
    
    * Chore: Theater button
    
    * Feat: Set player dimension in different modes
    
    * Feat: Hide/show icons
    
    * Feat: JS to toggle theater mode
    
    * Feat: JS to toggle full screen mode
    
    * Feat: JS to toggle mini player mode
    
    * Feat: Keyboard shortcuts for different modes
    
    * Refactor: Keyboard accessibility improvements

--- src/player.js
+++ src/player.js
@@ -1,21 +1,52 @@
 window.onload=function(){
   const playPauseBtn = document.querySelector(".play-pause-btn")
+  const theaterBtn = document.querySelector(".theater-btn")
+  const fullScreenBtn = document.querySelector(".full-screen-btn")
+  const miniPlayerBtn = document.querySelector(".mini-player-btn")
   const videoContainer = document.querySelector(".video-container")
   const video = document.querySelector("video")
+
+  // View mode
+  theaterBtn.addEventListener("click", toggleTheaterMode)
+  fullScreenBtn.addEventListener("click", toggleFullScreenMode)
+  miniPlayerBtn.addEventListener("click", toggleMiniPlayerMode)
+
+  function toggleTheaterMode() {
+    videoContainer.classList.toggle("theater")
+  }
+
+  function toggleFullScreenMode() {
+    if (document.fullscreenElement == null) {
+      videoContainer.requestFullscreen()
+    } else {
+      document.exitFullscreen()
+    }
+  }
+
+  function toggleMiniPlayerMode() {
+    if (videoContainer.classList.contains("mini-player")) {
+      document.exitPictureInPicture()
+    } else {
+      video.requestPictureInPicture()
+    }
+  }
+
+  document.addEventListener("fullscreenchange", () => {
+    videoContainer.classList.toggle("full-screen", document.fullscreenElement)
+  })
+
+  document.addEventListener("enterpictureinpicture", () => {
+    videoContainer.classList.add("mini-player")
+  })
+
+  document.addEventListener("leavepictureinpicture", () => {
+    videoContainer.classList.remove("mini-player")
+  })
 
   // Play or pause 
   const togglePlay = () => {
     video.paused ? video.play() : video.pause()
   }
-
-  document.addEventListener("keydown", e => {
-    switch(e.key.toLowerCase()) {
-      case " ":
-      case "k":
-        togglePlay()
-        break
-    }
-  })
 
   playPauseBtn.addEventListener("click", togglePlay)
   video.addEventListener("click", togglePlay)
@@ -26,5 +57,28 @@ window.onload=function(){
 
   video.addEventListener("pause", () => {
     videoContainer.classList.add("paused")
+  })
+
+  document.addEventListener("keydown", e => {
+    const tagName = document.activeElement.tagName.toLowerCase()
+
+    if (tagName === "input") return
+
+    switch(e.key.toLowerCase()) {
+      case " ":
+        if (tagName === "button") return
+      case "k":
+        togglePlay()
+        break
+      case "f":
+        toggleFullScreenMode()
+        break
+      case "t":
+        toggleTheaterMode()
+        break
+      case "i":
+        toggleMiniPlayerMode()
+        break
+    }
   })
 }
