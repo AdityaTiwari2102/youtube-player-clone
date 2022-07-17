f6c02abf0c2ce812527610f584094540763046d7
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-07-17 15:48:51 +0530

    Feat: JS to toggle theater mode

--- src/player.js
+++ src/player.js
@@ -1,8 +1,15 @@
 window.onload=function(){
   const playPauseBtn = document.querySelector(".play-pause-btn")
+  const theaterBtn = document.querySelector(".theater-btn")
   const videoContainer = document.querySelector(".video-container")
   const video = document.querySelector("video")
 
+  // View mode
+  theaterBtn.addEventListener("click", toggleTheaterMode)
+
+  function toggleTheaterMode() {
+    videoContainer.classList.toggle("theater")
+  }
   // Play or pause 
   const togglePlay = () => {
     video.paused ? video.play() : video.pause()
